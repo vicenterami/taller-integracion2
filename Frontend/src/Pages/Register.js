6b155d4976104416fba1a738dedf730fd76a4f9d
branco <branco.garcia2020@alu.uct.cl>
2023-09-11 20:30:18 -0300

    register

--- Frontend/src/Pages/Register.js
+++ Frontend/src/Pages/Register.js
@@ -3,8 +3,40 @@ import React from "react";
 function Register() {
   return (
     <div>
-      <h2>Página de Registro</h2>
-      {/* Agrega contenido y formularios de registro aquí */}
+      <div>
+        
+      </div>
+
+      <div>
+        <h2>Registro</h2>
+        <form>
+        <div>
+          <label> Nombre y apellido</label>
+          <input type="text" placeholder="Ej: Juan Perez"></input>
+        </div>
+        <div>
+          <label>Rut</label>
+          <input type="" placeholder="1.111.111-1"></input>
+        </div>
+        <div>
+          <label>Correo electrónico</label>
+          <input type="email" placeholder="nombre@ejemplo.com"></input>
+        </div>
+        <div>
+          <label>Telefono</label>
+          <input type="" placeholder="9-12345678"></input>
+        </div>
+        <div>
+          <label>Contraseña</label>
+          <input type="" placeholder=""></input>
+        </div>
+        <div>
+          <label>Confirmar contraseña</label>
+          <input type="" placeholder=""></input>
+        </div>
+        <button type="submit"> Registrarse</button>
+        </form>
+      </div>
     </div>
   );
 }
