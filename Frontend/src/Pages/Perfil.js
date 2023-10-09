027a8d3c23b36bc3ec0348a1602c80029333539a
der-kaffe <jcardenas2022@alu.uct.cl>
2023-10-09 15:06:23 -0300

    perfil css

--- Frontend/src/Pages/Perfil.js
+++ Frontend/src/Pages/Perfil.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { useAuth } from "../AuthContext";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useAuth } from '../AuthContext'; // Replace with your actual auth library
+import logo from '../Images/logo.png'; // Replace with the actual path to your logo
 
 function Perfil() {
   const { userData } = useAuth();
@@ -22,33 +23,48 @@ function Perfil() {
   }, [userData]);
 
   return (
-    <div className="perfil-container d-flex align-items-center justify-content-center vh-100">
-        <div className="perfil-header border border-success p-5 text-center" >
-            <h2>Mi Perfil</h2>
-            
-        </div>
-      <div className="perfil-content border border-danger p-5 text-center" style={{ fontSize: "24px" }}>
-            
-            {data ? (
-                <div className="user-details">
-                <p>
-                    <strong>Nombre:</strong> {data.nombre}
-                </p>
-                <p>
-                    <strong>Rut:</strong> {data.rut}
-                </p>
-                <p>
-                    <strong>Correo:</strong> {data.correo}
-                </p>
-                <p>
-                    <strong>Teléfono:</strong> {data.telefono}
-                </p>
+    <section className="vh-100">
+      <div className="container py-5 h-100">
+        <div className="row justify-content-center align-items-center h-100">
+          <div className="col-lg-8 mb-4 mb-lg-0">
+            <div className="card mb-3" style={{ borderRadius: '.5rem' }}>
+              <div className="row g-0">
+                <div className="col-md-5 gradient-custom text-center"
+                  style={{ borderTopLeftRadius: '.5rem', borderBottomLeftRadius: '.5rem',  backgroundColor:'#007BFF' }}>
+                  <img src={logo} style={{ width: '180px', marginTop: '25px', borderRadius: '.5rem' }} alt="Logo" />
+
+                  <h5 className="card-title" style={{ color: 'black', marginTop: '20px', fontSize: '1.5rem' }}>{data.nombre}</h5>
+                  <i className="far fa-edit mb-3"></i>
                 </div>
-            ) : (
-                <p>Cargando...</p>
-            )}
+                <div className="col-md-7">
+                  <div className="card-body p-4">
+                    <h6>Informacion</h6>
+                    <hr className="mt-0 mb-4" />
+                    <div className="row pt-1">
+                      <div className="col-6 mb-3">
+                        <h6>Email</h6>
+                        <p className="text-muted" style={{ fontSize: '1.2rem' }}>{data.correo}</p>
+                      </div>
+                      <div className="col-6 mb-3">
+                        <h6>Numero</h6>
+                        <p className="text-muted" style={{ fontSize: '1.2rem' }}>{data.telefono}</p>
+                      </div>
+                    </div>
+                    <hr className="mt-0 mb-4" />
+                    <div className="row pt-1">
+                      <div className="col-6 mb-3">
+                        <h6>Rut</h6>
+                        <p className="text-muted" style={{ fontSize: '1.2rem' }}>{data.rut}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
