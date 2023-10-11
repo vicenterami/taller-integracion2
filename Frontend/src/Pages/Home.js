3dda804ea94ee235d9fb5b60ac7c78644d7c24dd
DburgosA <111473775+DburgosA@users.noreply.github.com>
2023-10-11 18:56:54 -0300

    Logout y cambios home

--- Frontend/src/Pages/Home.js
+++ Frontend/src/Pages/Home.js
@@ -19,7 +19,11 @@ function Home() {
     // Cambia la ruta a "/perfil" cuando se hace clic en el botón
     navigate("/perfil");
   };
-  
+      //logout
+  const handleLogout = () => {
+    localStorage.removeItem( userData);
+    navigate("/IniciarSesion");
+  };
 
   useEffect(() => {
     
@@ -46,6 +50,7 @@ function Home() {
               <li><a onClick={() => handlePageChange('Horas medicas')} style={{ textDecoration: 'none', color: 'white', fontWeight: 'bold' }}>Horas Medicas</a></li>
               <li><a onClick={() => handlePageChange('Medicamentos')} style={{ textDecoration: 'none', color: 'white', fontWeight: 'bold' }}>Medicamentos</a></li>
               <li><a onClick={() => handlePageChange('Evolucion medica')} style={{ textDecoration: 'none', color: 'white', fontWeight: 'bold' }}>Evolucion medica</a></li>
+              <li><button onClick={handleLogout} className="btn btn-danger btn-lg fs-4">Cerrar Sesión</button></li>
             </ul>
           </nav>
         </header>
@@ -54,7 +59,10 @@ function Home() {
       Ir a Perfil
     </button>
       <div id="contenidoHome" style={{ margin: "20px", fontSize: "18px" }}>
-        {data ? (
+        {/* Mostrar contenido según la página seleccionada */}
+        {selectedPage === 'inicio' && 
+        <div>
+          {data ? (
           <p>
             Nombre: {data.nombre}, rut: {data.rut}, correo:{" "}
             {data.correo}, telefono: {data.telefono}
@@ -63,10 +71,7 @@ function Home() {
           <p>Cargando...</p>
         )}
         <h2>Bienvenido a la Página de Usuario</h2>
-        <p>Este es el contenido principal de tu aplicación.</p>
-
-        {/* Mostrar contenido según la página seleccionada */}
-        {selectedPage === 'inicio' && <p>Contenido de Inicio</p>}
+        <p>Este es el contenido principal de tu aplicación.</p></div>}
         {selectedPage === 'Horas medicas' && 
           <div>
             <p>Contenido de Horas medicas</p>
