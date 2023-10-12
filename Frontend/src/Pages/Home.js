b68f3d20f1528d7e911612210753b4d80ab7127c
DburgosA <111473775+DburgosA@users.noreply.github.com>
2023-10-11 22:01:36 -0300

    Fragcionamiento pagina home
    se agrego nuevos archivos que contienen de manera separada las direcciones que contiene Home

--- Frontend/src/Pages/Home.js
+++ Frontend/src/Pages/Home.js
@@ -3,8 +3,10 @@ import { useLocation } from "react-router-dom";
 import { useAuth } from "../AuthContext";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import Citas from "./Citas"
-
+import Citas from "./utilidades/Citas"
+import Seguimiento from "./utilidades/Seguimiento";
+import Inicio from "./utilidades/Inicio";
+import Medicamento from "./utilidades/Medicamento";
 
 function Home() {
   const navigate = useNavigate();
@@ -19,14 +21,13 @@ function Home() {
     // Cambia la ruta a "/perfil" cuando se hace clic en el botón
     navigate("/perfil");
   };
-      //logout
+    //logout
   const handleLogout = () => {
     localStorage.removeItem( userData);
-    navigate("/IniciarSesion");
+    navigate("/");
   };
 
   useEffect(() => {
-    
     // Realizas una solicitud a la API para obtener datos del usuario
     axios.get(`http://45.236.129.38:3000/api/User/${userData.rut}`)
       .then((response) => {
@@ -43,43 +44,25 @@ function Home() {
       <div className="container mt-5" style={{ width: '100%', backgroundColor: '#007bff', color: 'white' }}>
         <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '16px' }}>
           <h1>Cefam</h1>
-          <h1>|||</h1>
+          <button onClick={handleIrAPerfil}>Perfil</button>
           <nav>
             <ul style={{ listStyle: 'none', display: 'flex', gap: '20px' }}>
               <li><a onClick={() => handlePageChange('inicio')} style={{ textDecoration: 'none', color: 'white', fontWeight: 'bold' }}>Inicio</a></li>
               <li><a onClick={() => handlePageChange('Horas medicas')} style={{ textDecoration: 'none', color: 'white', fontWeight: 'bold' }}>Horas Medicas</a></li>
               <li><a onClick={() => handlePageChange('Medicamentos')} style={{ textDecoration: 'none', color: 'white', fontWeight: 'bold' }}>Medicamentos</a></li>
-              <li><a onClick={() => handlePageChange('Evolucion medica')} style={{ textDecoration: 'none', color: 'white', fontWeight: 'bold' }}>Evolucion medica</a></li>
+              <li><a onClick={() => handlePageChange('Seguimiento')} style={{ textDecoration: 'none', color: 'white', fontWeight: 'bold' }}>Seguimiento</a></li>
               <li><button onClick={handleLogout} className="btn btn-danger btn-lg fs-4">Cerrar Sesión</button></li>
             </ul>
           </nav>
         </header>
       </div>
-      <button onClick={handleIrAPerfil}>
-      Ir a Perfil
-    </button>
+
       <div id="contenidoHome" style={{ margin: "20px", fontSize: "18px" }}>
         {/* Mostrar contenido según la página seleccionada */}
-        {selectedPage === 'inicio' && 
-        <div>
-          {data ? (
-          <p>
-            Nombre: {data.nombre}, rut: {data.rut}, correo:{" "}
-            {data.correo}, telefono: {data.telefono}
-          </p>
-        ) : (
-          <p>Cargando...</p>
-        )}
-        <h2>Bienvenido a la Página de Usuario</h2>
-        <p>Este es el contenido principal de tu aplicación.</p></div>}
-        {selectedPage === 'Horas medicas' && 
-          <div>
-            <p>Contenido de Horas medicas</p>
-            <Citas/>
-          </div>
-        }
-        {selectedPage === 'Medicamentos' && <p>Contenido de Medicamentos</p>}
-        {selectedPage === 'Evolucion medica' && <p>Contenido de Evolucion medica</p>}
+        {selectedPage === 'inicio' && <div><Inicio/></div>}
+        {selectedPage === 'Horas medicas' && <div><Citas/></div>}
+        {selectedPage === 'Medicamentos' && <div><Medicamento/></div>}
+        {selectedPage === 'Seguimiento' && <div><Seguimiento/></div>}
       
       </div>
     </>
