238c8579371a44dce72b724d41b52c3bac7be461
Vicente-Ramirez <vramirez2020@alu.uct.cl>
2023-11-20 19:11:12 -0300

    juntar lo de juan

--- Frontend/src/Pages/Reservar.js
+++ Frontend/src/Pages/Reservar.js
@@ -1,13 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useAuth } from '../AuthContext';
-import ArrowComponent from './utilidades/BackArrow';
-import { Toaster, toast } from 'sonner'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useAuth } from "../AuthContext";
+import ArrowComponent from "./utilidades/BackArrow";
+import { Toaster, toast } from "sonner";
 
 const Reservar = () => {
   const { userData } = useAuth();
   const [citasDisponibles, setCitasDisponibles] = useState([]);
-  const [citaSeleccionada, setCitaSeleccionada] = useState(null);
 
   useEffect(() => {
     obtenerCitasDisponibles();
@@ -15,7 +14,9 @@ const Reservar = () => {
 
   const obtenerCitasDisponibles = async () => {
     try {
-      const response = await axios.get('http://45.236.129.38:3000/api/citas/disponibles');
+      const response = await axios.get(
+        "http://45.236.129.38:3000/api/citas/disponibles"
+      );
       const citas = response.data.citas;
       setCitasDisponibles(citas);
     } catch (error) {
@@ -25,64 +26,61 @@ const Reservar = () => {
 
   const reservarCita = async (citaId) => {
     try {
-      const response = await axios.post('http://45.236.129.38:3000/api/citas/reservar', {
-        citaId,
-        pacienteId: userData.rut,
-      });
+      const response = await axios.post(
+        "http://45.236.129.38:3000/api/citas/reservar",
+        {
+          citaId,
+          pacienteId: userData.rut,
+        }
+      );
 
       if (response.status === 200) {
         obtenerCitasDisponibles();
-        toast.success('¡La cita se ha registrado correctamente!')
+        toast.success("¡La cita se ha registrado correctamente!");
       } else {
-        console.error('Error al reservar la cita:', response.data.message);
-        toast.error('¡Algo ha fallado a la hora de registrar la cita!')
-        
+        console.error("Error al reservar la cita:", response.data.message);
+        toast.error("¡Algo ha fallado a la hora de registrar la cita!");
       }
     } catch (error) {
-      console.error('Error al reservar la cita:', error.message);
-      toast.error('¡Algo ha fallado a la hora de registrar la cita!')
+      console.error("Error al reservar la cita:", error.message);
+      toast.error("¡Algo ha fallado a la hora de registrar la cita!");
     }
   };
 
-
   return (
     <div className="container">
-      <ArrowComponent to="/Home"/>
+      <ArrowComponent to="/Home" />
       <div className="text-center">
         <h1>Horas Médicas Disponibles</h1>
         <p>{userData.nombre}, Elige tu Hora</p>
 
-        <h2>Citas Disponibles:</h2>
+        <h2>Citas Disponibles: </h2>
         <div className="row justify-content-center">
           {citasDisponibles.map((cita) => (
-            <div
-              key={cita._id}
-              className="col-md-4 mb-3" 
-              onClick={() => setCitaSeleccionada(cita)}
-            >
-              <div
-                className='card border-primary'
-                style={{ padding: '10px' }}
-              >
+            <div key={cita._id} className="col-md-4 mb-3">
+              <div className="card border-primary" style={{ padding: "10px" }}>
                 <p>Doctor: {cita.doctor}</p>
                 <p>Especialidad: {cita.especialidad}</p>
                 <p>Fecha: {cita.fecha}</p>
-                <p>Disponible: {cita.disponible ? 'Sí' : 'No'}</p>
-                <Toaster richColors position="top-center" toastOptions={{style: { width:'100%' , height:'auto' },    }}/> 
-                {(
+                <p>Disponible: {cita.disponible ? "Sí" : "No"}</p>
+                <p>{cita._id}</p>
+                <Toaster
+                  richColors
+                  position="top-center"
+                  toastOptions={{ style: { width: "100%", height: "auto" } }}
+                />
+                {
                   <button
                     className="btn btn-primary"
-                    
-                    onClick={() => reservarCita(citaSeleccionada._id)}
+                    onClick={() => reservarCita(cita._id)}
                   >
                     Reservar Cita
                   </button>
-                )}
+                }
               </div>
             </div>
           ))}
         </div>
-
       </div>
     </div>
   );
